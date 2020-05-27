b88109f2f43b5cb3fa274f4bb8381a311d34f10a
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-05-27 03:27:15 -0300

    :art: ui improvements

--- src/components/shared/Form.js
+++ src/components/shared/Form.js
@@ -1,4 +1,19 @@
 import styled from "@emotion/styled";
 
-export const Input = styled.input``;
-export const Button = styled.button``;
+export const Input = styled.input`
+  padding: 0.5rem 2.1rem;
+  font-size: 1.6rem;
+  flex: 4;
+  border: 1px solid #ddd;
+`;
+
+export const Button = styled.button`
+  flex: 1;
+  color: #fff;
+  padding: 0.5rem 2.1rem;
+  background-color: dodgerblue;
+  border: 1px solid dodgerblue;
+  font-weight: 600;
+  cursor: pointer;
+  font-size: 1.6rem;
+`;
--- src/components/shared/Logo.js
+++ src/components/shared/Logo.js
@@ -4,9 +4,9 @@ import React from "react";
 export const LogoText = styled.h1`
   font-family: "Open Sans", sans-serif;
   font-size: 10rem;
-
-  color: grey;
-  margin: 0;
+  color: #333340;
+  margin-top: 6rem;
+  margin-bottom: 2rem;
 `;
 
 const Logo = () => {
