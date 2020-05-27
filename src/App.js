202154e78d3e7474c1dee87e54e3832d3ca2db88
Tales Pereira <42183799+talesdsp@users.noreply.github.com>
2020-05-26 22:04:47 -0300

    introduce apollo provider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache } from "@apollo/client";
+import React from "react";
+import Planets from "./components/Planets";
+import Search from "./components/Search";
 
-function App() {
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({ uri: "https://hasura-planets.herokuapp.com/v1/graphql" }),
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Search />
+      <Planets />
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
